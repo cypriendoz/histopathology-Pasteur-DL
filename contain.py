9ae9cd3ea2ad90212aefa96a9676f73885875a0c
cypriendoz <30530854+cypriendoz@users.noreply.github.com>
2017-07-28 17:44:54 +0200

    Add files via upload

---
+++ contain.py
@@ -0,0 +1,25 @@
+def inside_polygon(x, y, points):
+    """
+    Return True if a coordinate (x, y) is inside a polygon defined by
+    a list of verticies [(x1, y1), (x2, x2), ... , (xN, yN)].
+
+    Reference: http://www.ariel.com.au/a/python-point-int-poly.html
+    """
+    n = len(points)
+    #m=str(n)
+    #print(m)
+    if n!=0:
+
+        inside = False
+        p1x, p1y = points[0]
+        for i in range(1, n + 1): #int from 1 to n (included)
+            p2x, p2y = points[i % n]
+            if y > min(p1y, p2y):
+                if y <= max(p1y, p2y):
+                    if x <= max(p1x, p2x):
+                        if p1y != p2y:
+                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                        if p1x == p2x or x <= xinters:
+                            inside = not inside
+            p1x, p1y = p2x, p2y
+        return inside
